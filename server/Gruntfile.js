8197f7a7bbb88aa8c21f812a7e6dfe3b01aea8b4
Nathan Craddock <nkcraddock@gmail.com>
2014-08-09 11:58:22 -0400

    Wired up node-inspector and added the debug target

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -6,12 +6,31 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-nodemon');
-
+	grunt.loadNpmTasks('grunt-node-inspector');
+	grunt.loadNpmTasks('grunt-open');
 
 	grunt.initConfig({
+		'node-inspector': {
+			dev: {
+				options: {
+					'web-port': 1338,
+					'web-host': 'localhost',
+					'save-live-edit': true,
+					'hidden': ['node_modules']
+				}
+			}
+		},
+		open: {
+			debug: {
+				path: 'http://localhost:1338/debug?port=5858'
+			}
+		},
 		nodemon: {
 			dev: {
-				script: 'app.js'
+				script: 'app.js',
+				options: {
+					nodeArgs: ['--debug']
+				}
 			}
 		},
 		concurrent: {
@@ -19,6 +38,13 @@ module.exports = function(grunt) {
 				'nodemon',
 				'jshint',
 				'watch'
+			],
+			debug: [
+				'nodemon',
+				'jshint',
+				'watch',
+				'node-inspector',
+				'open:debug'
 			],
 			options: {
 				logConcurrentOutput: true
@@ -40,6 +66,7 @@ module.exports = function(grunt) {
 		}
 	});
 
-	grunt.registerTask('default', ['concurrent']);
+	grunt.registerTask('default', ['concurrent:dev']);
+	grunt.registerTask('debug', ['concurrent:debug']);
 
 };
